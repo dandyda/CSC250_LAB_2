312a027ccc06e8b63fb9cf6adef399f74c8efe7b
dandyda <46979665+dandyda@users.noreply.github.com>
2019-03-11 05:47:31 -0500

    Update playArrayCharII.c

--- playArrayCharII.c
+++ playArrayCharII.c
@@ -1,268 +1,285 @@
+/*Dino Dan, CSC250-SP19
+  Contributed by Noah and Gary
+  3/11/19*/
+
+
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include "gfx2.h"
 #include "lab1-p2-letterLib.h"
 #define SIZE 100
 
-
 int main()
 {
     
-     //sets screen size 500 x 500
-	int ysize = 600;
-	int xsize = 600;
+     //sets screen size 800 x 800
+	int ySize = 800;
+	int xSize = 800;
     
-    double userX;
-    double userY;
-    double userHeight;
+    long int r, g, b;
+    double ulCornerX, ulCornerY, height;
 
-	// Open a new window for drawing.
-	gfx_open(xsize,ysize,"Example Graphics Program");
-
-	// Set the current drawing color to red
-	gfx_color(200,0,0);
-	
-	
-    char userStringInput[100];//the array name is 'UserStringInput' and can hold 99 characters and one null termination character
+	char c, userStringInput[SIZE];//Initializing the array named 'UserStringInput' and can hold 99 characters and one null termination character
            
-    printf("%s", "Enter a single word in all caps:\n");// prompts user input
+    printf("Enter a word:\n");// prompts user input
     scanf("%s", userStringInput);
     
-    //printf("%s", "Enter a color for your string:\n");
-    //scanf("%s", userColor);
-    //printf("%d", "Enter a height:\n");
-    //scanf("%d", userHeight);
-    //printf("%d,%d", "Enter x and y coordinates:\n");
-    //scanf("%d,%d", userX, userY);
-    /*Introducing a prototype function called print, it's job is to print the user's input stored in array 'input'
-      **the function prototype print is using a character pointer called 'string' to copy data stored in 'input' array.
-      **this allows us to manipulate data in other functions without changing user input.*/
-    //variable 'i' is being used as a watchdog for the null termination character '\0'.
-    int i = 0;
-    char c;
-    while (userStringInput[i] != '\0')
-    {   
-        //printf("%c\n", userString[i]);//this will print each element on terminal and put a new line after each element at terminal but not as drawChar graphic
-        //i++;//this will increment through each element of array 'input' and copy data to 'string'
+    // ask user for the red color value 0-255
+	printf("Enter the RED color value (0-255): ");
+	scanf("%ld", &r);
+	
+	// ask user for the green color value 0-255
+	printf("Enter the GREEN color value (0-255): ");
+	scanf("%ld", &g);
+	
+	// ask user for the blue color value 0-255
+	printf("Enter the BLUE color value (0-255): ");
+	scanf("%ld", &b);
     
-    //this for statement checks each character in userStringInput and implements the character to the cases in switch statement
-    for( i=0; i < SIZE; i++ ) {
+    printf("Enter a height:\n");
+    scanf("%lf", &height);
     
-    //tests userStringInput for each character against each case until the while statement encounters null terminator
-    switch (userStringInput[i]) {
+    printf("Enter x starting coordinate:\n");
+    scanf("%lf", &ulCornerX);
+    
+    printf("Enter y starting coordinate:\n");
+    scanf("%lf", &ulCornerY);
+    
+    // Open a new window for drawing.
+	gfx_open(xSize,ySize,"Graphics Letter Library");
+	gfx_color(r,g,b);// Set the current drawing color to user preference
+    
+    // loop for case switch
+    for(int i=0; i<SIZE; i++){
+    	c = userStringInput[i];
+    	ulCornerX += .8*height;
+    	
+    //use a grey box to test each character drawn
+    	gfx_color(100,100,100);
+    
+    //tests userStringInput for each character against each case until the while statement encounters null terminator	
+        gfx_color(r,g,b);     
+        switch (c) {
+                     
+            /*case 'A':
+            case 'a':
+                drawCharA(ulCornerX,ulCornerY,height);
+                break; */
             
+            /*case 'B':
+            case 'b':
+                drawCharB(ulCornerX,ulCornerY,height);
+                break; */
+                
+            case 'C':
+            case 'c':
+                drawCharC(ulCornerX,ulCornerY,height);
+                break;
+                
+            case 'D':
+            case 'd':
+                drawCharD(ulCornerX,ulCornerY,height);
+                break;
+            
+            case 'E':
+            case 'e':
+                drawCharE(ulCornerX,ulCornerY,height);
+                break;
+            
+            /*case 'F':
+            case 'f':
+                drawCharF(ulCornerX,ulCornerY,height);
+                break; */
+            
+            /*case 'G':
+            case 'g':
+                drawCharG(ulCornerX,ulCornerY,height);
+                break; */
+                
+            case 'H':
+            case 'h':
+                drawCharH(ulCornerX,ulCornerY,height);
+                break;
+                
+            case 'I':
+            case 'i':
+                drawCharI(ulCornerX,ulCornerY,height);
+                break;
+            
+            case 'J':
+            case 'j':
+                drawCharJ(ulCornerX,ulCornerY,height);
+                break;
+            
+            /*case 'K':
+            case 'k':
+                drawCharK(ulCornerX,ulCornerY,height);
+                break; */
+            
+            case 'L':
+            case 'l':
+                drawCharL(ulCornerX,ulCornerY,height);
+                break;
+            
+            case 'M':
+            case 'm':
+                drawCharM(ulCornerX,ulCornerY,height);
+                break;
+            
+            case 'N':
+            case 'n':
+                drawCharN(ulCornerX,ulCornerY,height);
+                break;
+            
+            /*case 'O':
+            case 'o':
+                drawCharO(ulCornerX,ulCornerY,height);
+                break; */
+            
+            /*case 'P':
+            case 'p':
+                drawCharP(ulCornerX,ulCornerY,height);
+                break; */
+                
+            case 'Q':
+            case 'q':
+                drawCharQ(ulCornerX,ulCornerY,height);
+                break;
+                
+            case 'R':
+            case 'r':
+                drawCharR(ulCornerX,ulCornerY,height);
+                break;
+                
+            case 'S':
+            case 's':
+                drawCharS(ulCornerX,ulCornerY,height);
+                break;
+                
+            case 'T':
+            case 't':
+                drawCharT(ulCornerX,ulCornerY,height);
+                break;
+                
+            /*case 'U':
+            case 'u':
+                drawCharU(ulCornerX,ulCornerY,height);
+                break; */
+                
+            case 'V':
+            case 'v':
+                drawCharV(ulCornerX,ulCornerY,height);
+                break;
+                
+            /*case 'W':
+            case 'w':
+                drawCharW(ulCornerX,ulCornerY,height);
+                break; */
+                
+            /*case 'X':
+            case 'x':
+                drawCharX(ulCornerX,ulCornerY,height);
+                break; */
+                
+            case 'Y':
+            case 'y':
+                drawCharY(ulCornerX,ulCornerY,height);
+                break;
+                
+            case 'Z':
+            case 'z':
+                drawCharZ(ulCornerX,ulCornerY,height);
+                break;
+            
+            /*case '0':
+                drawChar0(ulCornerX,ulCornerY,height);
+                break; */
+                
+            /*case '1':
+                drawChar1(ulCornerX,ulCornerY,height);
+                break; */
+                
+            case '2':
+                drawChar2(ulCornerX,ulCornerY,height);
+                break;
+                
+            /*case '3':
+                drawChar3(ulCornerX,ulCornerY,height);
+                break; */
+                
+            case '4':
+                drawChar4(ulCornerX,ulCornerY,height);
+                break;
+                
+            /*case '5':
+                drawChar5(ulCornerX,ulCornerY,height);
+                break; */
+                
+            /*case '6':
+                drawChar6(ulCornerX,ulCornerY,height);
+                break; */
+                
+            /*case '7':
+                drawChar7(ulCornerX,ulCornerY,height);
+                break; */
+            
+            case '8':
+                drawChar8(ulCornerX,ulCornerY,height);
+                break;
+                
+            /*case '9':
+                drawChar9(ulCornerX,ulCornerY,height);
+                break; */
+            
+            //cases below ignore new lines, tabs, spaces in array    
+            case '\n': 
+            case '\t':
+            case ' ':
+            case '\0':
+               break;
+            
+            /*case '$':
+                drawChar$(0,0,100);
+                break;
+            
+            case '!':
+                drawChar!(0,0,100);
+                break;
+            
+            case '?':
+                drawCharQuestion(0,0,100);
+                break;
         
-        /*case "clear":
-        case "Clear":
-            something();
-            break;
+            case '#':
+                drawChar#(0,0,100);
+                break;*/
+                
+            default:
+                drawMissingLetter(ulCornerX,ulCornerY,height);
+                break;  
         
-        case "exit":
-        case "Exit":
-            something();
-            break;*/
+            /*case "clear":
+            case "Clear":
+                something();
+                break;
         
-        case 'A':
-        case 'a':
-            drawCharA(0,0,100);
-            break;
+            case "exit":
+            case "Exit":
+                something();
+                break;*/
             
-        case 'B':
-        case 'b':
-            drawCharB(0,0,100);
-            break;
-            
-        case 'C':
-        case 'c':
-            drawCharC(0,0,100);
-            break;
-            
-        case 'D':
-        case 'd':
-            drawCharD(0,0,100);
-            break;
-            
-        case 'E':
-        case 'e':
-            drawCharE(15,15,30);
-            break;
-            
-        case 'F':
-        case 'f':
-            drawCharF(0,0,100);
-            break;
-            
-        case 'G':
-        case 'g':
-            drawCharG(0,0,100);
-            break;
-            
-        case 'H':
-        case 'h':
-            drawCharH(0,0,100);
-            break;
-            
-        case 'I':
-        case 'i':
-            drawCharI(0,0,100);
-            break;
-            
-        case 'J':
-        case 'j':
-            drawCharJ(0,0,100);
-            break;
-            
-        case 'K':
-        case 'k':
-            drawCharK(0,0,100);
-            break;
-            
-        case 'L':
-        case 'l':
-            drawCharL(0,0,100);
-            break;
-            
-        case 'M':
-        case 'm':
-            drawCharM(0,0,100);
-            break;
-            
-        case 'N':
-        case 'n':
-            drawCharN(0,0,100);
-            break;
-            
-        case 'O':
-        case 'o':
-            drawCharO(0,0,100);
-            break;
-            
-        case 'P':
-        case 'p':
-            drawCharP(0,0,100);
-            break;
-            
-        case 'Q':
-        case 'q':
-            drawCharQ(10,10,30);
-            break;
-            
-        case 'R':
-        case 'r':
-            drawCharR(20,20,30);
-            break;
-            
-        case 'S':
-        case 's':
-            drawCharS(0,0,100);
-            break;
-            
-        case 'T':
-        case 't':
-            drawCharT(25,25,30);
-            break;
-            
-        case 'U':
-        case 'u':
-            drawCharU(0,0,100);
-            break;
-            
-        case 'V':
-        case 'v':
-            drawCharV(0,0,100);
-            break;
-            
-        case 'W':
-        case 'w':
-            drawCharW(15,15,30);
-            break;
-            
-        case 'X':
-        case 'x':
-            drawCharX(0,0,100);
-            break;
-            
-        case 'Y':
-        case 'y':
-            drawCharY(30,30,30);
-            break;
-            
-        case 'Z':
-        case 'z':
-            drawCharZ(0,0,100);
-            break;
-        
-        case '0':
-            drawChar0(0,0,100);
-            break;
-            
-        case '1':
-            drawChar1(0,0,100);
-            break;
-            
-        case '2':
-            drawChar2(0,0,100);
-            break;
-            
-        case '3':
-            drawChar3(0,0,100);
-            break;
-            
-        case '4':
-            drawChar4(0,0,100);
-            break;
-            
-        case '5':
-            drawChar5(0,0,100);
-            break;
-            
-        case '6':
-            drawChar6(0,0,100);
-            break;
-            
-        case '7':
-            drawChar7(0,0,100);
-            break;
-        
-        case '8':
-            drawChar8(0,0,100);
-            break;
-            
-        case '9':
-            drawChar9(0,0,100);
-            break;
-            
-        /*case '$':
-            drawChar$(0,0,100);
-            break;
-            
-        case '!':
-            drawChar!(0,0,100);
-            break;
-            
-        case '?':
-            drawCharQuestion(0,0,100);
-            break;
-        
-        case '#':
-            drawChar#(0,0,100);
-            break;*/
-            
-        default:
-        drawMissingLetter(50,50,50);
-        break;
-            
-                    
+                              
         }//end switch
     }//end for
     
-    // Wait for the user to press a character.
+    while(1) {
+		// Wait for the user to press a character.
 		c = gfx_wait();
 
 		// Quit if it is the letter q.
 		if(c=='q') break;
-	}//End while	 
+	}//end while 
 	
-      		
+	return 0;    		
 	
 } //End main function
