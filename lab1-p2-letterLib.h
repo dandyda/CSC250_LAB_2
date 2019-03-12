67c9af0410eb9855935e820079cd28e06148b918
dandyda <46979665+dandyda@users.noreply.github.com>
2019-03-11 20:54:46 -0500

    Create lab1-p2-letterLib.h

---
+++ lab1-p2-letterLib.h
@@ -0,0 +1,53 @@
+//Header file with prototype function for drawing letter 'Q'
+//Dino Dan CSC250-SP19
+// Any Line commented out means function does not operate normally
+
+void drawText(char* userStringInput, int r, int g, int b, double ulCornerX, double ulCornerY, double height);
+
+void drawCharA(double upperleftx, double upperlefty, double height);
+void drawCharB(double upperleftx, double upperlefty, double height);
+void drawCharC(double upperleftx, double upperlefty, double height);
+void drawCharD(double ulCornerX, double ulCornerY, double height);
+void drawCharE( double ulCornerX, double ulCornerY, double height );
+void drawCharF(double upperleftx, double upperlefty, double height);
+void drawCharG(double upperleftx, double upperlefty, double height);
+void drawCharH(double ulCornerX, double ulCornerY, double height);
+void drawCharI( double ulCornerX, double ulCornerY, double height);
+void drawCharJ(double ulCornerX,double ulCornerY,double height);
+void drawCharK(double upperleftx, double upperlefty, double height);
+void drawCharL(double ulCornerX, double ulCornerY, double height);
+void drawCharM(double ulCornerX, double ulCornerY, double height);
+void drawCharN(double ulCornerX, double ulCornerY, double height);
+void drawCharO(double upperleftx, double upperlefty, double height);
+void drawCharP(double upperleftx, double upperlefty, double height);
+void drawCharQ(double ulCornerX, double ulCornerY, double height);
+void drawCharR(double ulCornerX, double ulCornerY, double height);
+void drawCharS(double ulCornerX, double ulCornerY, double height);
+void drawCharT(double ulCornerX,double ulCornerY, double height);
+void drawCharU(double upperleftx, double upperlefty, double height);
+void drawCharV(double ulCornerX, double ulCornerY, double Height);
+void drawCharW(double upperleftx, double upperlefty, double height);
+void drawCharX(double upperleftx, double upperlefty, double height);
+void drawCharY(double ulCornerX, double ulCornerY, double side);
+void drawCharZ (double ulCornerX, double ulCornerY, double height);
+
+void drawMissingLetter(double ulCornerX, double ulCornerY, double height);
+
+//void drawCharDollar(double ulCornerX, double ulCornerY, double height)
+void drawCharQuestion(double upperleftx, double upperlefty, double height);
+//void drawChar!(double upperleftx, double upperlefty, double height);
+//void drawChar#(double upperleftx, double upperlefty, double height);
+
+void drawChar0(double upperleftx, double upperlefty, double height);
+void drawChar1(double upperleftx, double upperlefty, double height);
+void drawChar2(double ulCornerX, double ulCornerY, double height);
+void drawChar3(double ulCornerX, double ulCornerY, double height);
+void drawChar4(double ulCornerX, double ulCornerY, double height);
+void drawChar5(double upperleftx, double upperlefty, double height);
+void drawChar6(double upperleftx, double upperlefty, double height);
+void drawChar7(double upperleftx, double upperlefty, double height);
+void drawChar8(double xUL, double yUL, double height);
+void drawChar9(double upperleftx, double upperlefty, double height);
+
+
+
